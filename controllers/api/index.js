44424f44312d807b5f128c10987f0832e1f527f8
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-05-29 17:38:12 -0700

    debud controllers api index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,14 @@
+//REQUIRE
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
+//USE
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
+//EXPORT
 module.exports = router;
